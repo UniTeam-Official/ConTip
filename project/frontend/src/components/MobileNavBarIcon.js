4c1d01c2358fe24e6e9ef48395080a6bedae261e
Yyr3LL <tarasenko.yura.dr@gmail.com>
2020-03-30 13:54:36 +0300

    wrote some react components

---
+++ project/frontend/src/components/MobileNavBarIcon.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        zdarova pacani
+      </div>
+    );
+  }
+}
+
+export default Header;
