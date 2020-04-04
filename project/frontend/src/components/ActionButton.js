b98868dc684273195f01bf5a5a0c00a622d573d2
kesha001 <berdanka006@gmail.com>
2020-04-05 00:01:37 +0300

    added liginform and action button components

--- project/frontend/src/components/ActionButton.js
+++ project/frontend/src/components/ActionButton.js
@@ -2,14 +2,17 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 
-class Header extends Component {
+class ActionButton extends Component {
   render() {
     return (
-      <div>
-        zdarova pacani
-      </div>
+      <div class="col-6 col-12-medium">
+				<ul class="actions stacked">
+					<li><a href="#" class="button small fit">Delete Account</a></li>
+					<li id="actionButton"><a href="#" class="button small fit">Delete Account</a></li>
+				</ul>
+			</div>
     );
   }
 }
 
-export default Header;
+export default ActionButton;
