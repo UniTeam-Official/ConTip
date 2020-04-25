3a6761c2dcb63c015c5549242588a10a61c353da
Yyr3LL <tarasenko.yura.dr@gmail.com>
2020-04-25 21:54:35 +0300

    made project/urls.py great again after incorrect merge

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_jwt import views as jwt_views
+import rest_framework_simplejwt.views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/account/', include('djoser.urls')),
-    path('api/v1/auth/login/', jwt_views.obtain_jwt_token, name='auth'),
+    path('api/v1/auth/login/', jwt_views.TokenObtainPairView.as_view(), name='auth'),
+    path('api/v1/auth/verify/', jwt_views.TokenVerifyView.as_view(), name='verify'),
+    path('api/v1/auth/refresh/', jwt_views.TokenRefreshView.as_view(), name='refresh'),
     path('api/v1/app/', include("app.urls")),
     path('', include("frontend.urls")),
 ]
