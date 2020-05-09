a02f8cd2058588c44aa7f753357c4494b9dea4e5
Yyr3LL <tarasenko.yura.dr@gmail.com>
2020-05-09 16:25:50 +0300

    Removed frontend app and cleaned some files from redundant imports, comments and fixed indentations

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,18 +1,3 @@
-"""project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 import rest_framework_simplejwt.views as jwt_views
@@ -24,5 +9,4 @@ urlpatterns = [
     path('api/v1/auth/verify/', jwt_views.TokenVerifyView.as_view(), name='verify'),
     path('api/v1/auth/refresh/', jwt_views.TokenRefreshView.as_view(), name='refresh'),
     path('api/v1/app/', include("app.urls")),
-    path('', include("frontend.urls")),
 ]
